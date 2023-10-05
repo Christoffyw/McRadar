ccd04a734962b44a0f1a40d539c0eb4ffbf9a79c
Christoffyw <weinhardtchristopher@gmail.com>
2023-10-05 11:09:30 -0400

    Initial Commit

---
+++ McRadar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  McRadar
+//
+//  Created by Christopher Weinhardt on 2023-10-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
